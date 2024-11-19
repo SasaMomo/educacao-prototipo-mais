e9bb54db293d650f298005d2d7d551ff25f548ff
Lucas <83623947+SasaMomo@users.noreply.github.com>
2024-11-19 01:50:46 -0300

    Update GoogleDocs.tsx

--- src/pages/GoogleDocs.tsx
+++ src/pages/GoogleDocs.tsx
@@ -7,9 +7,12 @@ export default function GoogleDocs() {
       <div className="bg-pri-mold w-full">
         <Header />
         <div className=" bg-[#ff000066] max-w-[1000px] mx-auto">
-        <h1>s</h1>
+          <h1 className="text-[26px] text-center">
+            Aqui ficarão as aulas e arquivos auxiliares
+          </h1>
+          <div className="h-[70vh]"></div>
+          <Footer />
         </div>
-        <Footer />
       </div>
     </>
   );
