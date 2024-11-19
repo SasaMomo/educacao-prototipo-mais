1e27642aff69bb32b309b630d865bc97cbb065ec
Lucas <83623947+SasaMomo@users.noreply.github.com>
2024-11-19 00:57:58 -0300

    TailWind Fix

--- src/pages/Contato.tsx
+++ src/pages/Contato.tsx
@@ -1,14 +1,43 @@
 import Link from "next/link";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import { DocumentArrowDownIcon } from "@heroicons/react/16/solid";
 
 export default function Contato() {
   return (
     <div className="bg-pri-mold">
       <Header />
       <div className="max-w-[1000px] mx-auto text-center">
-        <h1>dasda</h1>
-        <p>d</p>
+        <div className="mx-auto text-center text-md sm:text-2xl rounded-md p-4 gap-y-2 flex flex-col min-h-[90vh]">
+          <p className="hover:underline hover:scale-110">
+            Email: sasa.lhcl@gmail.com
+          </p>
+          <p className="hover:underline hover:scale-110">
+            Telefone: (85) 98419-5751
+          </p>
+          <p className="hover:underline hover:scale-110">
+            <a target="_blank" href="https://www.linkedin.com/in/lucas-sasa/">
+              Linkedin: www.linkedin.com/lucas-sasa/
+            </a>
+          </p>
+          <p className="hover:underline hover:scale-110 ">
+            <a
+              href="https://wa.me/+5585984195751?text=Olá%20Lucas,%20eu%20gostaria%20de%20falar%20com%20você%20sobre%20o%20seu%20site!"
+              target="_blank"
+            >
+              Whatsapp:  
+              <strong className="text-blue-400">Clique aqui</strong>
+            </a>
+          </p>
+          <Link
+            href="https://drive.google.com/drive/folders/1Ih_CHLrvP9srEwcjVPrKBTSvNKh7xx5u?usp=sharing"
+            target="_blank"
+            className="flex mx-auto cursor-pointer hover:scale"
+          >
+            <p className="py-1 text-md sm:text-4xl">Acesse o Google Drive:  </p>
+            <DocumentArrowDownIcon className="w-10 sm:w-16 hover:scale-[1.50]" />
+          </Link>
+        </div>
         <Footer />
       </div>
     </div>
--- src/pages/Materias.tsx
+++ src/pages/Materias.tsx
@@ -7,15 +7,15 @@ export default function Materias() {
   return (
     <div className=" w-full bg-pri-mold">
       <Header />
-      <div className="flex flex-col mx-auto text-center max-w-[1000px] bg-[#ff000066]">
+      <div className="flex flex-col mx-auto text-center max-w-[1000px] bg-[#ff000066] gap-y-2">
         <h1 className="text-[32px]">Matérias</h1>
-        <h2 className="text-[22px]">Documentos Google</h2>
-        <h3>Como faço para começar?</h3>
+        <h2 className="text-[22px] border-t-2">Documentos Google</h2>
+        <h3>Usado para criar documentos na nuvem.</h3>
         <p>
-          Para começar é muito simples, primeiro de tudo você precisar criar uma
-          conta google ou entrar em uma conta que você ja tenha.
+          Você ira aprender como fazer login/criar sua conta do Google, acessar
+          os produtos do Google, como usar o Google drive, criar diversos tipos
+          de documentos na nuvem, como usa-los e compartilhar-los.
         </p>
-        <p>Passo 1:</p>
         <Image
           className="mx-auto"
           src="https://i.imgur.com/FIPZGKe.png"
@@ -29,10 +29,86 @@ export default function Materias() {
         >
           Clique para acessar
         </Link>
-        <h2 className="text-[22px]">Inteligência Artificial</h2>
-        <h2 className="text-[22px]">Programação Scratch</h2>
-        <h2 className="text-[22px]">Programação Roblox Studio</h2>
-        <h2 className="text-[22px]">Canva</h2>
+        <h2 className="text-[22px] border-t-2">Inteligência Artificial</h2>
+        <h3>Ferramentas IA</h3>
+        <p>
+          Você aprenderá quais as ferramentas IA mais usadas no mercado e como
+          usar cada uma delas por exemplo:
+        </p>
+        <p>Passo 1:</p>
+        <Image
+          className="mx-auto"
+          src="https://i.imgur.com/oEdU5lu.png"
+          alt="IA Icon"
+          width={300}
+          height={100}
+        ></Image>
+        <Link
+          href="/GoogleDocs"
+          className="border-2 rounded-md py-2 px-4 min-w-[240px] mx-auto my-2 text-[22px] bg-pri-red hover:bg-pri-green hover:scale-110 active:animate-ping"
+        >
+          Clique para acessar
+        </Link>
+        <h2 className="text-[22px] border-t-2">Programação Scratch</h2>
+        <h3>Seu inicio na jornada da programação</h3>
+        <p>
+          Você aprenderá sobre lógica de programação e como criar seus primeiros
+          jogos através de blocos e palavras sem a necessidade de códigos.
+        </p>
+        <p>Passo 1:</p>
+        <Image
+          className="mx-auto"
+          src="https://i.imgur.com/BsTrtor.png"
+          alt="Scratch Icon"
+          width={300}
+          height={100}
+        ></Image>
+        <Link
+          href="/GoogleDocs"
+          className="border-2 rounded-md py-2 px-4 min-w-[240px] mx-auto my-2 text-[22px] bg-pri-red hover:bg-pri-green hover:scale-110 active:animate-ping"
+        >
+          Clique para acessar
+        </Link>
+        <h2 className="text-[22px] border-t-2">Programação Roblox Studio</h2>
+        <h3>Como faço para começar?</h3>
+        <p>
+          Para começar é muito simples, primeiro de tudo você precisar criar uma
+          conta google ou entrar em uma conta que você ja tenha.
+        </p>
+        <p>Passo 1:</p>
+        <Image
+          className="mx-auto"
+          src="https://i.imgur.com/gx1A3mz.png"
+          alt="Roblox Studio Icon"
+          width={300}
+          height={100}
+        ></Image>
+        <Link
+          href="/GoogleDocs"
+          className="border-2 rounded-md py-2 px-4 min-w-[240px] mx-auto my-2 text-[22px] bg-pri-red hover:bg-pri-green hover:scale-110 active:animate-ping"
+        >
+          Clique para acessar
+        </Link>
+        <h2 className="text-[22px] border-t-2">Canva</h2>
+        <h3>Como faço para começar?</h3>
+        <p>
+          Para começar é muito simples, primeiro de tudo você precisar criar uma
+          conta google ou entrar em uma conta que você ja tenha.
+        </p>
+        <p>Passo 1:</p>
+        <Image
+          className="mx-auto"
+          src="https://i.imgur.com/aSeLhFV.png"
+          alt="Canva Icon"
+          width={190}
+          height={100}
+        ></Image>
+        <Link
+          href="/GoogleDocs"
+          className="border-2 rounded-md py-2 px-4 min-w-[240px] mx-auto my-2 text-[22px] bg-pri-red hover:bg-pri-green hover:scale-110 active:animate-ping"
+        >
+          Clique para acessar
+        </Link>
         <Footer />
       </div>
     </div>
